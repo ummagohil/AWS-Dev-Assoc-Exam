65abe11316ccf9961f2470cfd2d23d3784643ab3
ummagohil <umgo0510@gmail.com>
2024-08-15 16:54:45 +0100

    deloyment issues

--- mocks/handlers.ts
+++
@@ -1,7 +0,0 @@
-import { rest } from "msw";
-
-export const handlers = [
-  rest.get("/api/user", (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json({ username: "John Doe" }));
-  }),
-];
